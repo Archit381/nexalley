c142cdb72a26fdbca99bbf9e390ef1dcc829862c
Archit <architojha420@gmail.com>
2023-12-23 16:15:11 +0530

    completed menu sidepage

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import styles from './menu.module.css'
+import React from "react";
+import styles from "./menu.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 function Menu() {
   return (
-    <div className={styles.container}>Menu</div>
-  )
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's Hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+
+      <MenuPosts withImage={false}/>
+
+      <h2 className={styles.subtitle}>{"Discover by topic"}</h2>
+      <h1 className={styles.title}>Categories</h1>
+
+      <MenuCategories/>
+
+      <h2 className={styles.subtitle}>{"Chosen by the Author"}</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+
+      <MenuPosts withImage={true}/>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import Syles from './footer.module.css'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className={Syles.container}>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image
+            style={{ borderRadius: 50, height: 40, width: 40 }}
+            src="/archit1.jpg"
+            alt="Archit Ojha"
+            width={50}
+            height={50}
+          />
+          <h1 className={styles.logoText}>Written By Archit Ojha</h1>
+        </div>
+        <p className={styles.desc}>
+          With a passion for programming, I am always seeking opportunities to
+          learn and grow.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
